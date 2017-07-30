b302ab02da9a7ea23601dfb0498679670cb1f866
qinghe <areq22@gmail.com>
2017-07-30 18:29:03 +0800

    Initialize

---
+++ WpfApplication/Pages/FirstPage.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication.Pages
+{
+    /// <summary>
+    /// Interaction logic for FirstPage.xaml
+    /// </summary>
+    public partial class FirstPage : Page
+    {
+        public FirstPage()
+        {
+            InitializeComponent();
+        }
+
+        private void mediaPlayer_Loaded(object sender, RoutedEventArgs e)
+        {
+            this.mediaPlayer.Play();
+
+        }
+
+        private void mediaPlayer_MediaOpened(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            //关闭视频，释放内存
+            var uri = new Uri("pack://application:,,,/Pages/MainPage.xaml");
+            this.NavigationService.Navigate(uri);
+            this.mediaPlayer.Close();
+        }
+
+        private void mediaPlayer_MediaEnded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //this.mediaPlayer.Play();
+
+        }
+    }
+}
---
+++ WpfApplication/Pages/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication.Pages
+{
+    /// <summary>
+    /// Interaction logic for MainPage.xaml
+    /// </summary>
+    public partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+
+        private void businessButton_Click(object sender, RoutedEventArgs e)
+        {
+            var uri = new Uri("pack://application:,,,/Pages/Business/IndexPage.xaml");
+            this.NavigationService.Navigate(uri);
+        }
+    }
+}
